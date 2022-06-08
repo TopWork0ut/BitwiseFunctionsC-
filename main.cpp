066e4334a58e28d2ebb9a5861404d7a609fd6a4f
TopWork0ut <olegiksen@gmail.com>
2022-06-08 10:34:19 +0300

    modified main.cpp with deleting of some comments

--- main.cpp
+++ main.cpp
@@ -21,35 +21,3 @@ int main() {
     return 0;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//int findOdd(int arr[], int n)
-//{
-//    int res = 0, i;
-//    for (i = 0; i < n; i++)
-//        res ^= arr[i];
-//        cout << res << endl;
-//    return res;
-//}
-
-// Driver Method
-//int main(void) {
-//    int arr[] = { 12, 12, 14, 90, 14, 14, 14,19 };
-//    int n = sizeof(arr) / sizeof(arr[0]);
-//    cout << "The odd occurring element is  "<< findOdd(arr, n);
-//    return 0;
-//}
